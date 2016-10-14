14fa468eefe89b0a03fefd1601d1a7b47e178adb
AudricPandele <audric.pandele@ingesup.com>
2016-10-14 08:51:39 +0200

    Register

--- www/js/controllers/AppCtrl.js
+++ www/js/controllers/AppCtrl.js
@@ -10,17 +10,34 @@ app.controller('AppCtrl', function($scope, $ionicModal, $timeout, $http, $sessio
   $ionicModal.fromTemplateUrl('templates/login.html', {
     scope: $scope
   }).then(function(modal) {
-    $scope.modal = modal;
+    $scope.modalLogin = modal;
+  });
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/register.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modalRegister = modal;
   });
 
   // Triggered in the login modal to close it
   $scope.closeLogin = function() {
-    $scope.modal.hide();
+    $scope.modalLogin.hide();
   };
 
   // Open the login modal
   $scope.login = function() {
-    $scope.modal.show();
+    $scope.modalLogin.show();
+  };
+
+  // Open the register modal
+  $scope.register = function() {
+    $scope.modalRegister.show();
+  };
+
+  // Triggered in the login modal to close it
+  $scope.closeRegister = function() {
+    $scope.modalRegister.hide();
   };
 
   // Perform the login action when the user submits the login form
@@ -42,6 +59,38 @@ app.controller('AppCtrl', function($scope, $ionicModal, $timeout, $http, $sessio
         $scope.closeLogin();
         $session.setUser(response.data.user.id);
         $state.go('app.single', {id: response.data.user.id}, {reload: true});
+      }
+
+    }, function errorCallback(response) {
+    });
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doRegister = function() {
+    $http({
+      method: 'POST',
+      url: 'http://localhost:1337/auth/signup',
+      data: {
+        username: $scope.loginData.username,
+        firstName: $scope.loginData.firstName,
+        lastName: $scope.loginData.lastName,
+        email: $scope.loginData.email,
+        password: $scope.loginData.password
+      }
+    }).then(function successCallback(response) {
+      if (response.data == null) {
+        $scope.errorLogin = true;
+      }else{
+        $scope.closeRegister();
+        $scope.login();
+
+
+        // $session.put('token', response.data.token);
+        // $session.put('id_session', response.data.user.id);
+        // $scope.errorLogin = false;
+        // $scope.closeLogin();
+        // $session.setUser(response.data.user.id);
+        // $state.go('app.single', {id: response.data.user.id}, {reload: true});
       }
 
     }, function errorCallback(response) {
